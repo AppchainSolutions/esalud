076eb409a766f5f1be5ecc4da06dccb767e0aee4
Omar Ahumada <omar.ahumadag@gmail.com>
2024-08-19 13:12:15 -0400

    Eliminacion de algunos archivos

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -22,48 +22,5 @@ Route::middleware([
 
     Route::inertia('/', 'Dashboard');
 
-    Route::get(
-        'personasPorEmpresa',
-        [DashboardController::class, 'personasPorEmpresa']
-    )->name('personasPorEmpresa');
-
-    Route::get(
-        'personasPorUnidad',
-        [DashboardController::class, 'personasPorUnidad']
-    )->name('personasPorUnidad');
-
-    Route::get(
-        'personasPorPlanta',
-        [DashboardController::class, 'personasPorPlanta']
-    )->name('personasPorPlanta');
-
-    Route::get(
-        'personasPorCeco',
-        [DashboardController::class, 'personasPorCeco']
-    )->name('personasPorCeco');
-
-    Route::get(
-        'personasPorExpo',
-        [DashboardController::class, 'personasPorExpo']
-    )->name('personasPorExpo');
-
-    Route::get(
-        'expo_empresa',
-        [DashboardController::class, 'expo_empresa']
-    )->name('expo_empresa');
-
-    Route::get(
-        'expo_planta',
-        [DashboardController::class, 'expo_planta']
-    )->name('expo_planta');
-
-    Route::get(
-        'enfermedad_empresa',
-        [DashboardController::class, 'enfermedad_empresa']
-    )->name('enfermedad_empresa');
-
-    Route::get(
-        'enfermedades_planta',
-        [DashboardController::class, 'enfermedades_planta']
-    )->name('enfermedades_planta');
+    
 });
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use App\Http\Controllers\AlergiaController;
 use App\Http\Controllers\AntecedenteFamiliarController;
 use App\Http\Controllers\AtencionDiariaController;
-use App\Http\Controllers\CertificacionController;
 use App\Http\Controllers\CirugiaController;
-use App\Http\Controllers\DiatController;
-use App\Http\Controllers\DiepController;
 use App\Http\Controllers\EnfermedadController;
 use App\Http\Controllers\FactorRiesgoController;
-use App\Http\Controllers\LicenciaMedicaController;
 use App\Http\Controllers\MedicamentoController;
 use App\Http\Controllers\PacienteController;
 use App\Http\Controllers\VacunaController;
@@ -49,21 +45,16 @@ Route::middleware([
 
     Route::prefix('/admin')->group(__DIR__ . '/admin/admin.php');
     Route::prefix('/consulta')->group(__DIR__ . '/consulta.php');
-    Route::prefix('/examen')->group(__DIR__ . '/examen.php');
     Route::prefix('/secundaria')->group(__DIR__ . '/secundaria.php');
     Route::prefix('/dashboard')->group(__DIR__ . '/dashboard.php');
 
     Route::inertia('/ficha', 'SubPages/FichaMedica');
     Route::resource('/alergia', AlergiaController::class);
     Route::resource('/atencion_diaria', AtencionDiariaController::class);
-    Route::resource('/certificacion', CertificacionController::class);
     Route::resource('/cirugia', CirugiaController::class);
-    Route::resource('/diat', DiatController::class);
-    Route::resource('/diep', DiepController::class);
     Route::resource('/enfermedad', EnfermedadController::class);
     Route::resource('/factor', FactorRiesgoController::class);
     Route::resource('/familiar', AntecedenteFamiliarController::class);
-    Route::resource('/licencia', LicenciaMedicaController::class);
     Route::resource('/medicamento', MedicamentoController::class);
     Route::resource('/paciente', PacienteController::class);
     Route::resource('/vacuna', VacunaController::class);

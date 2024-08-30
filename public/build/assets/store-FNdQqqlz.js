5768cbbafc74316b5ff06a6530d8b2681d06580c
Omar Ahumada <omar.ahumadag@gmail.com>
2024-08-30 18:22:31 -0400

    Ult Compilacion

---
+++ public/build/assets/store-FNdQqqlz.js
@@ -0,0 +1,2 @@
+import{M as t}from"./app-DryNi66c.js";const a=t("DataStore",{state:()=>({selected:{}}),getters:{getSelected:e=>e.selected},actions:{async setUserSelected(e){state.userSelected=e}}});export{a as u};
+//# sourceMappingURL=store-FNdQqqlz.js.map

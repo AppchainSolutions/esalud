8aefe60a956c617228b1c67339c15cbaffff2d5c
Omar Ahumada <omar.ahumadag@gmail.com>
2024-09-01 16:29:41 -0400

    Incluye archivos compilados

---
+++ bootstrap/ssr/assets/Create-Dyh5WaHJ.js
@@ -0,0 +1,58 @@
+import { mergeProps, withCtx, createVNode, useSSRContext } from "vue";
+import { ssrRenderComponent } from "vue/server-renderer";
+import { A as AppLayout } from "./AppLayout-C6vazPI6.js";
+import CreateTeamForm from "./CreateTeamForm-BpLu_gKT.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "@inertiajs/vue3";
+import "./logo-DLUxz0ei.js";
+import "./FormSection-B5keodaR.js";
+import "./SectionTitle-DVlhFTWU.js";
+import "./TextInput-C7v4Ykkw.js";
+import "./InputLabel-C6a7Opn8.js";
+import "./PrimaryButton-BB-VszM2.js";
+const _sfc_main = {
+  __name: "Create",
+  __ssrInlineRender: true,
+  setup(__props) {
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(ssrRenderComponent(AppLayout, mergeProps({ title: "Create Team" }, _attrs), {
+        header: withCtx((_, _push2, _parent2, _scopeId) => {
+          if (_push2) {
+            _push2(`<h2 class="font-semibold text-xl text-gray-800 leading-tight"${_scopeId}> Create Team </h2>`);
+          } else {
+            return [
+              createVNode("h2", { class: "font-semibold text-xl text-gray-800 leading-tight" }, " Create Team ")
+            ];
+          }
+        }),
+        default: withCtx((_, _push2, _parent2, _scopeId) => {
+          if (_push2) {
+            _push2(`<div${_scopeId}><div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8"${_scopeId}>`);
+            _push2(ssrRenderComponent(CreateTeamForm, null, null, _parent2, _scopeId));
+            _push2(`</div></div>`);
+          } else {
+            return [
+              createVNode("div", null, [
+                createVNode("div", { class: "max-w-7xl mx-auto py-10 sm:px-6 lg:px-8" }, [
+                  createVNode(CreateTeamForm)
+                ])
+              ])
+            ];
+          }
+        }),
+        _: 1
+      }, _parent));
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Pages/Teams/Create.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+const Create = /* @__PURE__ */ _export_sfc(_sfc_main, [["__file", "/home/blueshadows/www/vulco/resources/js/Pages/Teams/Create.vue"]]);
+export {
+  Create as default
+};
+//# sourceMappingURL=Create-Dyh5WaHJ.js.map

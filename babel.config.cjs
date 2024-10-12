bce7c95c13b16b05a0512a4de4207831a527a3bb
Blue <omar.ahumadag@gmail.com>
2024-10-11 23:31:44 -0300

    archivos eleiminados
    Eliminación de carpetas y archivos no esenciales para producción

--- babel.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: [
-        "@babel/preset-env",
-        "@vue/babel-preset-jsx", // Add this if you are using JSX
-    ],
-    plugins: [
-        // Add any Babel plugins if needed
-    ],
-};

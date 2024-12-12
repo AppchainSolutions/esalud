7da4240f7e381263169b115494bb598c46d712f3
Omar Ahumada G. <omar.ahumadag@gmail.com>
2024-12-12 20:57:47 -0300

    Refactoring Tools.php

--- app/Http/Controllers/ExAlcoholController.php
+++ app/Http/Controllers/ExAlcoholController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Repository\ExAlcoholRepository;
-tro
+
 class ExAlcoholController extends Controller
 {
     protected $exalcoholRepository;

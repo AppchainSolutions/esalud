26ed0b91f72e97983a133b82f8fe730ed2533624
Omar Ahumada <omar.ahumadag@gmail.com>
2025-02-08 11:24:33 -0300

    Configurar workspace y mocks para testing

---
+++ vitest.workspace.js
@@ -0,0 +1,10 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  "./vendor/laravel/framework/src/Illuminate/Foundation/resources/exceptions/renderer/vite.config.js",
+  "./vendor/laravel/jetstream/stubs/livewire/vite.config.js",
+  "./vendor/laravel/jetstream/stubs/inertia/vite.config.js",
+  "./vite.config.js",
+  "./vite.config.ts",
+  "./tests/JavaScript/vitest.config.js"
+])

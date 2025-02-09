1675799a38df38e17c4ec37eddf28a3760aa6da1
Omar Ahumada <omar.ahumadag@gmail.com>
2025-02-09 13:25:28 -0300

    Feat: Agregar llamada a DatabaseMasterSeeder en UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Paciente;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Helpers\RutGenerator;
@@ -34,10 +35,23 @@ class UserSeeder extends Seeder
             'lastname' => 'User',
             'rut' => RutGenerator::generate(),
             'email' => 'test@example.com',
-            'isAdmin' => true,
+            'isAdmin' => false,
             'password' => Hash::make('clave123'),
-            'rol' => 'admin',
+            'rol' => 'paciente',
         ]);
+
+        
+
+        Paciente::factory()->create([
+            'nombre' => 'Test',
+            'apellidos' => 'User',
+            'rut' => RutGenerator::generate(),
+            'email' => 'test@example.com',
+            'cuenta_activada' => true,
+            'user_id' => 2
+        ]);
+
+        $this->call(DatabaseMasterSeeder::class);
 
         // Puedes agregar más usuarios según sea necesario
     }

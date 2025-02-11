f1490577b7cafe9ea5a4dd7e28fc5e8c6706f48c
Omar Ahumada <omar.ahumadag@gmail.com>
2025-02-11 18:31:31 -0300

    feat(workflow): Añadir guía de commits semánticos y validación de commits

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', 'fix', 'docs', 'style', 'refactor', 
+      'test', 'chore', 'perf', 'security'
+    ]],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case']
+  }
+};
